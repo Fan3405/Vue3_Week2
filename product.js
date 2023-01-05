7562cf9e8a75d4b64ea0bab9ef4be9918633b583
LAPTOP-56LICNUE\USER <>
2023-01-05 23:41:21 +0800

    第二週主線任務

---
+++ product.js
@@ -0,0 +1,42 @@
+const app = {
+  data() {
+    return {
+      apiPath: "zxcv123",
+      productDetail: {},
+      // 產品資料格式
+      products: [],
+    };
+  },
+  methods: {
+    checkAdmin() {
+      axios
+        .post(`https://vue3-course-api.hexschool.io/v2/api/user/check`)
+        .then((response) => {
+          this.getData();
+        });
+    },
+    getData() {
+      axios
+        .get(
+          `https://vue3-course-api.hexschool.io/v2/api/${this.apiPath}/admin/products`
+        )
+        .then((response) => {
+          this.products = response.data.products;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  mounted() {
+    // 取出 Token
+    const token = document.cookie.replace(
+      /(?:(?:^|.*;\s*)Token\s*=\s*([^;]*).*$)|^.*$/,
+      "$1"
+    );
+    axios.defaults.headers.common.Authorization = token;
+    // 每次都會帶入
+    this.checkAdmin();
+  },
+};
+Vue.createApp(app).mount("#app");
